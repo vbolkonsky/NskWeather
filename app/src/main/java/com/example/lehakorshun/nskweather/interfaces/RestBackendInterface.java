55c0ad18a7a2f40b6dde94fce501332b8be4d5fd
lehakorshun <lehakorshun@gmail.com>
2015-12-29 00:07:38 +0600

    add constant

--- app/src/main/java/com/example/lehakorshun/nskweather/interfaces/RestBackendInterface.java
+++ app/src/main/java/com/example/lehakorshun/nskweather/interfaces/RestBackendInterface.java
@@ -1,13 +1,14 @@
 package com.example.lehakorshun.nskweather.interfaces;
 
 import com.example.lehakorshun.nskweather.model.Mmweather;
+import com.example.lehakorshun.nskweather.utils.Constant;
 
 import retrofit.Call;
 import retrofit.http.GET;
 
 public interface RestBackendInterface {
 
-    @GET("/xml/29634_1.xml")
+    @GET(Constant.PATH)
     Call<Mmweather> getWeather();
 
 }
