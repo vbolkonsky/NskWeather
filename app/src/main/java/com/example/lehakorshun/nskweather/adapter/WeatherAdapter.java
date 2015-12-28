178bc2364370806792e28ee6e089221a01d2ccbe
lehakorshun <lehakorshun@gmail.com>
2015-12-28 13:55:32 +0600

    add images

---
+++ app/src/main/java/com/example/lehakorshun/nskweather/adapter/WeatherAdapter.java
@@ -0,0 +1,36 @@
+package com.example.lehakorshun.nskweather.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lehakorshun.nskweather.R;
+
+import butterknife.ButterKnife;
+
+public class WeatherAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recycler, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
