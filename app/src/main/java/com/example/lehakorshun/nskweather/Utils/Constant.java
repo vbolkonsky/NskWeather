14f6459776e02f632a5de6681242db65e8744726
lehakorshun <lehakorshun@gmail.com>
2015-12-29 00:56:03 +0600

    fix padding

--- app/src/main/java/com/example/lehakorshun/nskweather/Utils/Constant.java
+++ app/src/main/java/com/example/lehakorshun/nskweather/Utils/Constant.java
@@ -3,5 +3,5 @@ package com.example.lehakorshun.nskweather.utils;
 public interface Constant {
 
     String URL = "http://informer.gismeteo.ru";
-    String PATH = "/xml/29634_.xml";
+    String PATH = "/xml/29634_1.xml";
 }
